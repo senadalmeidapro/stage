461f5cef6ff274f1952949df7833f9717beef6b4
senadalmeidapro <senadalmeidapro@gmail.com>
2025-07-02 18:33:30 +0100

    deploy10

--- apps/children/serializers.py
+++ apps/children/serializers.py
@@ -1,43 +1,36 @@
 from rest_framework import serializers
 from apps.users.serializers import UserTypeSerializer
-from apps.users.models import UserType
-from .models import *
+from .models import Child
+
 
 class ChildSerializer(serializers.ModelSerializer):
-    parent = UserTypeSerializer(read_only=True)
-    read_only_fields = ['id']
+    parent = UserTypeSerializer(read_only=True)  # affichage uniquement, pas d’écriture
 
     class Meta:
         model = Child
-        fields = ['parent', 'id', 'last_name', 'first_name', 'birthday', 'detail', 'joined_date', 'existe']
-        read_only_fields = ['id', 'parent', 'joined_date']
+        fields = [
+            "parent",
+            "id",
+            "last_name",
+            "first_name",
+            "birthday",
+            "detail",
+            "joined_date",
+            "existe",
+        ]
+        read_only_fields = ["id", "parent", "joined_date"]
         extra_kwargs = {
-            'last_name': {'required': True},
-            'first_name': {'required': True},
-            'birthday': {'required': True},
-            'detail': {'required': False},
-            'existe': {'required': False},
+            "last_name": {"required": True},
+            "first_name": {"required": True},
+            "birthday": {"required": True},
+            "detail": {"required": False},
+            "existe": {"required": False},
         }
 
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields.pop('parent', None)
-        self.fields['joined_date'].read_only = True
-        if self.instance is not None:
-            self.fields['parent_id'].read_only = True
-            # self.fields['parent'].read_only = True
-    
-
-    def create(self, validated_data):
-        return super().create(validated_data)
-    
     def update(self, instance, validated_data):
-        validated_data.pop('parent', None)
+        # Empêche modification du parent même si transmis par erreur
+        validated_data.pop("parent", None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         return instance
-
-
--- apps/children/views.py
+++ apps/children/views.py
@@ -1,35 +1,31 @@
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
 from apps.users.models import UserType
-from .serializers import ChildSerializer, Child
+from .models import Child
+from .serializers import ChildSerializer
 
-
-
-class ChildViewSet(mixins.CreateModelMixin,
-                     mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.ListModelMixin,
-                     viewsets.GenericViewSet):
-    
+class ChildViewSet(viewsets.ModelViewSet):
     serializer_class = ChildSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
+            # Accès total pour le staff
             return Child.objects.select_related('parent__user')
+        # Pour les parents, seulement leurs enfants actifs
         return Child.objects.filter(parent__user=user, existe=True).select_related('parent__user')
-    
+
     def perform_create(self, serializer):
         profil = UserType.objects.get(user=self.request.user)
-        if profil.type !='parent':
-            return PermissionDenied("Seul un parent peut inscrire un enfant")
-        parent_id = profil.id
-        serializer.save(parent_id=parent_id)
+        if profil.type != 'parent':
+            raise PermissionDenied("Seul un parent peut inscrire un enfant")
+        # Injection automatique du parent lié à l'utilisateur connecté
+        serializer.save(parent=profil)
 
     def perform_update(self, serializer):
         child = self.get_object()
         user = self.request.user
         if child.parent.user != user:
-            raise PermissionDenied("Vous ne pouvez pas modifier les donnée de cet enfant.")
+            raise PermissionDenied("Vous ne pouvez pas modifier les données de cet enfant.")
         serializer.save()
