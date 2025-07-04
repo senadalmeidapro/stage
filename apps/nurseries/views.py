90297234bec7dbd2a0682507135372dc6572cb54
senadalmeidapro <senadalmeidapro@gmail.com>
2025-07-04 17:06:57 +0100

    Update views.py

--- apps/nurseries/views.py
+++ apps/nurseries/views.py
@@ -1,101 +1,92 @@
-from rest_framework import viewsets, mixins, permissions, status
-from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
-from .models import Nursery, NurseryAssistant, OpeningHour
-from .serializers import NurserySerializer, NurseryAssistantSerializer, OpeningHourSerializer
-from apps.users.models import UserType
-from .filters import NurseryFilter, NurseryPagination
-from django_filters import rest_framework as filters
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, JSONParser, FormParser
+from django.core.files.storage import default_storage
+from .models import Nursery, OpeningHour, NurseryAssistant
+from .serializers import NurserySerializer, OpeningHourSerializer, NurseryAssistantSerializer
 
-
-class NurseryGetViewSet(
-    mixins.RetrieveModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
-    permission_classes = [permissions.AllowAny]
-    queryset = Nursery.objects.filter(manager__type='nursery_manager', verified=True)
-    filter_backends = [filters.DjangoFilterBackend]
-    filterset_class = NurseryFilter
-    pagination_class = NurseryPagination
-
-    class BasicNurserySerializer(NurserySerializer):
-        class Meta(NurserySerializer.Meta):
-            fields = ['id', 'name', 'address']
-
-    class DetailedNurserySerializer(NurserySerializer):
-        class Meta(NurserySerializer.Meta):
-            fields = [
-                'id', 'name', 'address', 'contact_number', 'legal_status',
-                'max_age', 'max_children_per_class', 'photo_exterior',
-                'photo_interior', 'opening_hours', 'information'
-            ]
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return self.BasicNurserySerializer
-        if self.action == 'retrieve':
-            return self.DetailedNurserySerializer
-        return super().get_serializer_class()
-
-
-class NurseryViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+class NurseryViewSet(viewsets.ModelViewSet):
     serializer_class = NurserySerializer
-    permission_classes = [permissions.IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, JSONParser, FormParser]
+    queryset = Nursery.objects.all()
 
     def get_queryset(self):
         if self.request.user.is_staff:
-            return Nursery.objects.all()
-        return Nursery.objects.filter(manager__user=self.request.user)
+            return self.queryset
+        return self.queryset.filter(manager__user=self.request.user)
 
     def perform_create(self, serializer):
-        manager = UserType.objects.get(user=self.request.user)
-        serializer.save(manager=manager)
-
-    def perform_update(self, serializer):
-        nursery = self.get_object()
-        if nursery.manager.user != self.request.user:
-            raise PermissionDenied("Vous ne pouvez pas modifier cette crèche.")
-        serializer.save()
+        if hasattr(self.request.user, 'usertype'):
+            nursery = serializer.save(manager=self.request.user.usertype)
+            
+            # Création des horaires par défaut si fournis dans le payload initial
+            if 'opening_hours' in self.request.data:
+                hours_serializer = OpeningHourSerializer(
+                    data=self.request.data['opening_hours'],
+                    many=True,
+                    context={'nursery': nursery}
+                )
+                hours_serializer.is_valid(raise_exception=True)
+                hours_serializer.save(nursery=nursery)
 
     @action(
         detail=True,
-        methods=['post', 'put', 'patch'],
+        methods=['GET', 'POST', 'PUT', 'PATCH'],
         url_path='opening-hours',
         parser_classes=[JSONParser]
     )
     def opening_hours(self, request, pk=None):
         nursery = self.get_object()
-        serializer = OpeningHourSerializer(data=request.data, many=True)
+        
+        # Vérification des permissions
+        if nursery.manager.user != request.user and not request.user.is_staff:
+            return Response(
+                {"detail": "Permission refusée."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        # GET - Lister les horaires existants
+        if request.method == 'GET':
+            hours = nursery.opening_hours.all()
+            serializer = OpeningHourSerializer(hours, many=True)
+            return Response(serializer.data)
+
+        # POST/PUT/PATCH - Créer ou mettre à jour les horaires
+        serializer = OpeningHourSerializer(
+            data=request.data,
+            many=True,
+            context={'nursery': nursery}
+        )
         serializer.is_valid(raise_exception=True)
 
-        nursery.opening_hours.all().delete()
+        # Suppression des anciens horaires (pour PUT/PATCH)
+        if request.method in ['PUT', 'PATCH']:
+            nursery.opening_hours.all().delete()
 
-        for hou in serializer.validated_data:
-            OpeningHour.objects.create(nursery=nursery, **hou)
+        # Création des nouveaux horaires
+        serializer.save(nursery=nursery)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        
+        # Suppression des fichiers associés
+        for field in ['agreement_document', 'id_card_document', 
+                     'photo_exterior', 'photo_interior']:
+            file = getattr(instance, field)
+            if file and default_storage.exists(file.name):
+                default_storage.delete(file.path)
+        
+        return super().destroy(request, *args, **kwargs)
 
-
-class NurseryAssistantViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+class NurseryAssistantViewSet(viewsets.ModelViewSet):
     serializer_class = NurseryAssistantSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
+    permission_classes = [IsAuthenticated]
+    
     def get_queryset(self):
         nursery_pk = self.kwargs.get('nursery_pk')
         return NurseryAssistant.objects.filter(nursery_id=nursery_pk)
@@ -106,4 +97,8 @@ class NurseryAssistantViewSet(
 
     def perform_update(self, serializer):
         assistant = self.get_object()
+        if assistant.nursery.manager.user != self.request.user and not self.request.user.is_staff:
+            raise PermissionDenied(
+                "Permission refusée."
+            )
         serializer.save()
