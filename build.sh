f0596cf9ce894a06b3eaa5e3a53973703e3088ba
senadalmeidapro <senadalmeidapro@gmail.com>
2025-07-04 17:41:42 +0100

    crete user super

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
+
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
 echo "Loading fixtures from data.json..."
 python manage.py loaddata data.json
+
+echo "Checking/Creating superuser..."
+# Vérifie si l'utilisateur existe déjà
+if echo "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='cheche').exists())" | python manage.py shell | grep -q "True"; then
+    echo "Superuser 'cheche' already exists. Skipping creation."
+else
+    echo "Creating superuser..."
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('cheche', '', 'alibababj')" | python manage.py shell
+    echo "Superuser created:"
+    echo "Username: cheche"
+    echo "Password: alibababj"
+fi
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
