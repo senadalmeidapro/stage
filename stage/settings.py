06a05f02fc2295d7223f9aca868385b8145351a9
senadalmeidapro <senadalmeidapro@gmail.com>
2025-07-28 10:55:50 +0100

    Update settings.py

--- stage/settings.py
+++ stage/settings.py
@@ -78,7 +78,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'stage.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
